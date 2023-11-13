4369c2d5e1f3cb527d71c98a4701388bd2ac8e8d
Jack B <jack@jack-b.me>
2023-11-13 21:10:40 +0000

    Fix problems during migration

--- database/migrations/2023_11_13_203230_create_orders_table.php
+++ database/migrations/2023_11_13_203230_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->float('totalValue');
             
