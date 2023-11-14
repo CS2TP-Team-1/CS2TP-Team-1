cc509675bf8533a6176fff802b1a39d9b068575b
CyrusZaydan <147829561+CyrusZaydan@users.noreply.github.com>
2023-11-14 11:22:36 +0000

    styles.css implementation fix (php)

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,33 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <!-- 
-    <link rel="stylesheet" type="text/css"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
-    -->
     <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
-    <link rel = "stylesheet" type="text/css" href="resources/css/styles.css" />  
+    <link rel="stylesheet" type="text/css" href="<?php echo asset('css/styles.css') ?>" />
 
     <!-- adding library for icons for top right -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
 
-
 <body>
-    <!--icons-->
-    <div class="icons">
-        <ul>
-            <li>
-                <a href="#"><i class="fa fa-search"></i></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-user"></i></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-shopping-basket"></i></a>
-            </li>
-        </ul>
+    <li>
+        <a href="#"><i class="fa fa-user"></i></a>
+    </li>
+    <li>
+        <a href="#"><i class="fa fa-shopping-basket"></i></a>
+    </li>
+    </ul>
     </div>
 
     <nav>
@@ -36,24 +26,28 @@
         </div>
 
         <script>
-            $(document).ready(function(){
+            $(document).ready(function () {
                 $('#nav-placeholder').load('/navbar');
             });  
         </script>
     </nav>
-    
+
     <h1>Welcome to The Jewellery Store</h1>
     <pre>
 Here you can check search by categories and view 
 bestselling items.
     </pre>
     <h2>Categories</h2>
-    <a href="ring_search"> <img src="resources/images/homepage/ring.png" alt="Ring" style="width:200px;height:200px;"><a>
-    <a href="bracelet_search"><img src="resources/images/homepage/bracelet.png" alt="Bracelet" style="width:200px;height:200px;"><a>
-    <a href="earring_search"><img src="resources/images/homepage/earring.jpg" alt="Earring" style="width:200px;height:200px;"><a>
-    <a href="watch_search"><img src="resources/images/homepage/watch.jpg" alt="Watch" style="width:200px;height:200px;"><a>
-    <!--<img src="img_girl.jpg" alt="Necklace" style="width:200px;height:200px;">-->
-    <h2>Best-selling items</h2>
+    <a href="ring_search"> <img src="resources/images/homepage/ring.png" alt="Ring"
+            style="width:200px;height:200px;"><a>
+            <a href="bracelet_search"><img src="resources/images/homepage/bracelet.png" alt="Bracelet"
+                    style="width:200px;height:200px;"><a>
+                    <a href="earring_search"><img src="resources/images/homepage/earring.jpg" alt="Earring"
+                            style="width:200px;height:200px;"><a>
+                            <a href="watch_search"><img src="resources/images/homepage/watch.jpg" alt="Watch"
+                                    style="width:200px;height:200px;"><a>
+                                    <!--<img src="img_girl.jpg" alt="Necklace" style="width:200px;height:200px;">-->
+                                    <h2>Best-selling items</h2>
 
 </body>
 
