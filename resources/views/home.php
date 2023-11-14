1364627c4785ad5a88b717348b6f121a785114a4
Jack B <jack@jack-b.me>
2023-11-14 10:32:18 +0000

    Fixing issues with merge request #3

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< Updated upstream:home.html
     <!-- 
     <link rel="stylesheet" type="text/css"
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
     -->
     <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
     <link rel = "stylesheet" type="text/css" href="resources/css/styles.css" />  
-=======
-    <link rel = "stylesheet" type="text/css" href="style.css" />
-    
-
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet">
->>>>>>> Stashed changes:resources/views/home.php
 
     <!-- adding library for icons for top right -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
