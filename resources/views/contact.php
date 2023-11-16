60bdd740963f464e0bc6f7b5b1617c5b82a81c21
Tonui11 <230035780@aston.ac.uk>
2023-11-16 14:14:24 +0000

    Fixed navbar and made css work on contact page

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel = "stylesheet" type="text/css" href="style.css" />
-    <link rel="stylesheet" type="text/css"
+    <link rel="stylesheet" type="text/css" href="<?php echo asset('css/styles.css') ?>" />
+
+    <!--<link rel="stylesheet" type="text/css"
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-    />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet">
+    /> -->
+    
+    <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
 
     <!-- adding library for icons for top right -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,7 +16,7 @@
 
 <body>
     <!--icons-->
-    <div class="icons">
+   <!-- <div class="icons">
         <ul>
             <li>
                 <a href="#"><i class="fa fa-search"></i></a>
@@ -27,7 +28,32 @@
                 <a href="#"><i class="fa fa-shopping-basket"></i></a>
             </li>
         </ul>
+    </div>-->
+
+    <li>
+        <a href="#"><i class="fa fa-user"></i></a>
+    </li>
+    <li>
+        <a href="#"><i class="fa fa-shopping-basket"></i></a>
+    </li>
+    </ul>
     </div>
+
+
+    <nav>
+        <div id="nav-placeholder">
+
+        </div>
+
+        <script>
+            $(document).ready(function(){
+                $('#nav-placeholder').load('/navbar');
+            });   
+        </script>
+    </nav>
+
+    
+
 <div class="contact">
     <header><h1><u>contact</u></h1></header>
     <p>complete the form below and we will get back to you within 24 hours</p>
@@ -61,17 +87,7 @@
 
 </div>
 
-    <nav>
-        <div id="nav-placeholder">
-
-        </div>
-
-        <script>
-            $(function(){
-                $("nav-placeholder").load("/navBar.html");
-            });  
-        </script>
-    </nav>
+    
     
     
     
