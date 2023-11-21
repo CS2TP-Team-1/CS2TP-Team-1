5be435acb70d32d12f503cbbfc28fedc6797eb42
Jack B <jack@jack-b.me>
2023-11-21 11:01:07 +0000

    Update migrations to have full contact form

--- database/migrations/2023_11_13_205453_create_contact_form_table.php
+++ database/migrations/2023_11_13_205453_create_contact_form_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
+            $table->string('number');
             $table->string('email');
+            $table->string('product');
             $table->string('message');
         });
     }
