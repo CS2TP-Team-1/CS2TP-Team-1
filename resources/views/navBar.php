bca64f8677ed94671c67b40395ff50a6c5b2001a
Jack B <jack@jack-b.me>
2023-12-08 19:41:20 +0000

    Make logo image work on all pages.
    Subpages in folders (products pages) couldn't direcly access logo.png due to not being in the same folder. Added / to the path to reset to root directory.

--- resources/views/navBar.php
+++ resources/views/navBar.php
@@ -19,25 +19,26 @@
         <li>
             <a href="#"><i class="fa fa-user" style="color: #9b26b6;"></i></a>
         </li>
-        
+
         <!--log in/ log out button page -->
         <li>
             <div>
-                <?php 
+                <?php
                 if (Auth::check()) { ?>
                     <button class='logout' onclick = "/logout">Log Out</button>
                 <?php } else { ?>
-                    <button class='login' onclick = "/login">login</button>
+                    <button class='login' onclick = "/login">Login</button>
                 <?php } ?>
             </div>
         </li>
 
         <li>
             <input type="text" placeholder="Search...">
+            <input type="submit" value="Search" />
         </li>
         <li2>
             <div class="logoimg">
-                <img src="logo.png" alt="Ring" class="productImg" alt = ""  height="400" width="500">
+                <img src="/logo.png" alt="The Jewellery Store Logo" class=""  height="400" width="500">
             </div>
         </li2>
 
@@ -50,7 +51,7 @@
 <!-- nav bar -->
 <nav class="main-header">
     <ul>
-        <li><a href="/">Home</a></li>                
+        <li><a href="/">Home</a></li>
         <li><a href="/products">Products</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/contact">Contact Us</a></li>
