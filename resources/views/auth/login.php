b7451fc06794fa8ad94a60c5aa8165462cb0bf13
Jack B <jack@jack-b.me>
2023-12-10 15:42:10 +0000

    Update backend for login page

---
+++ resources/views/auth/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="<?php echo asset('/css/styles.css') ?>"/>
+
+    <!-- adding library for icons for top right -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+<body>
+<nav>
+    <div id="nav-placeholder">
+
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#nav-placeholder').load('/navbar');
+        });
+    </script>
+</nav>
+
+<form id="login-form" method="POST" action="<?php echo route('login-store') ?>">
+    <?php echo csrf_field(); ?>
+    <h2>Sign In</h2>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" required>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="submit">Sign In</button>
+</form>
+</body>
+</html>
--- resources/views/index.php
+++ resources/views/index.php
@@ -4,6 +4,7 @@
 use App\Models\Product;
 ?>
 <head>
+    <meta charset="UTF-8">
     <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
     <link rel = "stylesheet" type="text/css" href="<?php echo asset('css/styles.css')?>" />
 
@@ -11,6 +12,7 @@ use App\Models\Product;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Jewellery Store</title>
 </head>
 
 <body>
