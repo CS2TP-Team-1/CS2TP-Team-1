99f655ffb300aea6a4a9fc65320e79155a386f27
Jack B <jack@jack-b.me>
2023-12-10 21:25:06 +0000

    Changes to make site demo ready
    Last minute formatting adjustments. Make about us button not do anything to prevent a 404 during demo

--- resources/views/navBar.php
+++ resources/views/navBar.php
@@ -55,7 +55,7 @@ use Illuminate\Support\Facades\Auth;
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/products">Products</a></li>
-        <li><a href="/about">About Us</a></li>
+        <li><a href="#">About Us</a></li>
         <li><a href="/contact">Contact Us</a></li>
     </ul>
 </nav>
