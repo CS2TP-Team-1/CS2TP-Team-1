f0310fd86ee2e6159a9ad8a34d41a16d93f66fcc
Jack B <jack@jack-b.me>
2023-12-10 21:29:55 +0000

    Add favicon.ico

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
     <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
     <meta name="viewport" content="width-device-width, initial-scale = 1.0">
+    <link rel="icon" href="/favicon.ico" />
 
     <!-- adding library for icons for top right -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- resources/views/products/new.php
+++ resources/views/products/new.php
@@ -3,6 +3,7 @@
 <head>
     <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
     <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
+    <link rel="icon" href="/favicon.ico" />
 
     <!-- adding library for icons for top right -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
