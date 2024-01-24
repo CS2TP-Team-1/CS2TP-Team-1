be599cf42e45cbc3a7c0dab54c2fd18e91bf38c3
Jack B <jack@jack-b.me>
2024-01-24 22:04:36 +0000

    Account type defaults to 'user' on creation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,6 +32,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'accountType' => 'user'
         ]);
 
         event(new Registered($user));
