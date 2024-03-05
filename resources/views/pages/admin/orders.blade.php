7e80ceb4368e7c71f198a43e9c11179773eaaaef
Tonui11 <230035780@aston.ac.uk>
2024-03-05 01:49:42 +0000

    Front-end for the Admin-Orders Dashboard

---
+++ resources/views/pages/admin/orders.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.default')
+@section('title', 'Admin-Orders')
+
+@section('content')
+<section class="admin">
+        <div class="table">
+            <div class="table-body">
+                <h4 class="table-title">
+                    Users' Orders
+                </h4>
+
+                <table class="table-table-stripped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Order ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Order Status</th>
+                            <th scope="col">Total Value of Order: £</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($orders as $order)
+                        <tr>
+                            <th scope="row">{{$order->id}}</th>
+                            <td>{{$order->user->name}}</td>
+                            <td>{{$order->user->email}}</td>
+                            <td>
+                                <form id="dropStatus" action="{{route('admin.order.updateStatus', $order->id)}}" method="post">
+                                    @csrf
+                                    <select name="status" onchange="submitChange()">
+                                        <option value="Ordered" @if($order->status == 'Ordered') selected @endif>Ordered</option>
+                                        <option value="Processing" @if($order->status == 'Processing') selected @endif>Processing</option>
+                                        <option value="Shipped" @if($order->status == 'Shipped') selected @endif>Shipped</option>
+
+                                    </select>    
+                                </form>
+                                <script>
+                                    function submitChange(){
+                                        document.getElementById('dropStatus').submit();
+                                    }
+                                </script>
+                            </td>
+                            <td>{{$order->totalValue}}</td>
+
+                        </tr>
+                        @endforeach
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+@endsection
