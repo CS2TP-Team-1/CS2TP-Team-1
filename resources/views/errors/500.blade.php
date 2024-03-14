2101a1b0d58c95716c6c0d6867be7c90fb8d734b
CyrusZaydan <147829561+CyrusZaydan@users.noreply.github.com>
2024-03-14 10:26:33 +0000

    button and error page fix
    Buttons now are not based on 30% website width and are instead based on text length. Error page buttons are aligned

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,6 +5,7 @@
 
     <h1>Error 500: Oops! There was an error</h1>
     <h3>Something went wrong! Please check the url and try again.</h3>
-    <button class="button" onclick="history.back()">Go Back!</button>
-
+    <div class="form">
+        <button class="button" onclick="history.back()">Go Back!</button>
+    </div>
 @endsection
