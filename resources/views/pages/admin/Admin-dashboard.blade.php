27a143994eba57de27f6bcde12c8e6563c104816
Jack B <jack@jack-b.me>
2024-03-21 10:50:46 +0000

    Capitalisations

--- resources/views/pages/admin/Admin-dashboard.blade.php
+++ resources/views/pages/admin/Admin-dashboard.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.default')
-@section('title', 'Admin dashboard')
+@section('title', 'Admin Dashboard')
 
 @section('content')
     <h2>Admin Dashboard</h2>
     <div class="form">
         <div class="account-form">
             <p>Manage and edit products:</p><br>
-            <button class="button" onclick="location.href='/admin/products'">Manage products</button>
+            <button class="button" onclick="location.href='/admin/products'">Manage Products</button>
 
             <p>Manage and edit users:</p><br>
-            <button class="button" onclick="location.href='/admin/users'">Manage users</button>
+            <button class="button" onclick="location.href='/admin/users'">Manage Users</button>
 
             <p>Manage and edit orders :</p><br>
-            <button class="button" onclick="location.href='/admin/orders'">Manage orders</button>
+            <button class="button" onclick="location.href='/admin/orders'">Manage Orders</button>
 
             <p>Manage and edit discount codes :</p><br>
-            <button class="button" onclick="location.href='/admin/discounts'">Manage discounts</button>
+            <button class="button" onclick="location.href='/admin/discounts'">Manage Discounts</button>
 
             <p>Manage and edit contact submissions :</p><br>
-            <button class="button" onclick="location.href='/admin/contact'">Manage contact submissions</button>
+            <button class="button" onclick="location.href='/admin/contact'">Manage Contact Submissions</button>
 
             <p>Manage and edit returns :</p><br>
-            <button class="button" onclick="location.href='/admin/returns'">Manage returns</button>
+            <button class="button" onclick="location.href='/admin/returns'">Manage Returns</button>
         </div>
     </div>
 @endsection
